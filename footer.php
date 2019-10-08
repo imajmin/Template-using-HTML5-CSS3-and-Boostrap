f033718dce85d665cd159e32623a59381ddd860f
Md Ajmin Pradhan <mr.prodhna912@gmail.com>
2019-10-08 10:24:12 +0600

    Uploaded all the files of template

---
+++ footer.php
@@ -0,0 +1,43 @@
+<div class="bg-dark">
+	<div class="container">
+		<div class="row pt-5">
+			<div class="col-sm-3 footer-widget">
+				<h6 class="text-white">About Me</h6>
+				<p class="text-white">
+					Hi,
+					My name is Ajmin Pradhan and i'm a tech enthusiast.
+
+					When I was in class 2 I saw computer for the first time and from that moment i'm in love with it. I started learning about various things form class 4 using my family phone. Till now i'm learning through my own research
+
+					I love sharing my knowledge and this is the reason beyond creating this channel. I hope i'll be able to achieve my goal.
+					Have a good day
+				</p>
+			</div>
+			<div class="col-sm-3 footer-widget">
+				<h5 class="text-white"><span class="text-primary">| L</span>atest Posts</h5>
+				<ul>
+					<li><a href="">> How to install WordPress on a live server | cPanel WordPress Installation</a></li>
+				</ul>
+			</div>
+
+			<div class="col-sm-3 footer-widget">
+				<h5 class="text-white"><span class="text-primary">| C</span>ategories</h5>
+				<ul>
+					<li><a href="">> Wordpress</a></li>
+				</ul>
+			</div>
+			<div class="col-sm-3 footer-widget">
+				<h4 class="text-white">Subscribe Newsletter</h4>
+				<input type="text" class="form-control" placeholder="Email address">
+			</div>
+		</div>
+	</div>
+</div>
+<script src="js/jquery-3.4.1.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+
+<div class="bg-dark">
+	<p class="text-center text-white p-2 mb-0">macwp © 2019</p>
+</div>  
+</body>
+</html>
